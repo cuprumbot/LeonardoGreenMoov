48a29eee6769690dc63563f520f38a558803b193
Luis Jimenez <luisrrjjcc@gmail.com>
2015-06-15 20:48:38 -0600

    Cambio de disco

--- OculusHead/main.cpp
+++ OculusHead/main.cpp
@@ -8,8 +8,6 @@
 #include <chrono>
 #include <math.h>
 
-/* ola ke ase */
-
 /* VR */
 #include "OVR.h"
 
@@ -49,6 +47,7 @@ Serial* SP;
 /* Leap */
 int originalAngles[] = {0,0,0,0,0};
 char fingerAngles[] = {180,180,180,180,180};
+char armAngles[] = {45,115,90,40,90};
 
 /*
 	CLASE LeapListener
@@ -100,7 +99,7 @@ void LeapListener::onFrame(const Controller& controller) {
 	int armAngle;	
 	float handposz = hand.palmPosition()[2];
 	float lengthBicep = 250;
-	aux = handposz/lengthBicep; //Por si la division no esta en [-1, 1]
+	aux = handposz/lengthBicep;		//Por si la division no esta en [-1, 1]
 	if(aux > 1) {
 		armAngle = 0;
 	} else if (aux < -1) {
@@ -108,27 +107,26 @@ void LeapListener::onFrame(const Controller& controller) {
 	} else {
 		armAngle = asin(handposz/lengthBicep)*180/PI;	
 	}
-
-		//Da angulos de -90 a 90
+	//Da angulos de -90 a 90
 	if(armAngle < 0){
-		armAngle = abs(armAngle) + 90; //Para enfrente es negativo por lo tanto volverlo positivo y sumarte 90 del otro plano
+		armAngle = abs(armAngle) + 90;				//Para enfrente es negativo por lo tanto volverlo positivo y sumarte 90 del otro plano
 	} else {
-		armAngle = 90 - armAngle; //Ajustar el angulo porque da el complemento
+		armAngle = 90 - armAngle;					//Ajustar el angulo porque da el complemento
 	}
 	
 
-	//elbow 5 - 95
+	//Elbow 5 - 95
 	int armPitchAngle = arm.direction().pitch()*180/PI;
-		//Da angulos de -90 a 90
+	//Da angulos de -90 a 90
 	if (armPitchAngle < 0) {
-		armPitchAngle = 90 - abs(armPitchAngle); //negativo es abajo y da el complemento
+		armPitchAngle = 90 - abs(armPitchAngle);	//Negativo es abajo y da el complemento
 	} else {
-		armPitchAngle = armPitchAngle + 90; //positivo mas 90 de los negativos
+		armPitchAngle = armPitchAngle + 90;			//Positivo mas 90 de los negativos
 	}
 
 	if(armAngle > 89) {
-		armPitchAngle = armPitchAngle - (armAngle - 90); //Al levantar el brazo el angulo del codo cambia
-														//(-90 porque en 90 el codo esta normal en la posicion 90 del brazo)
+		armPitchAngle = armPitchAngle - (armAngle - 90);	//Al levantar el brazo el angulo del codo cambia
+															//-90 porque en 90 el codo esta normal en la posicion 90 del brazo
 	}		
 	if(armPitchAngle > 95) {
 		armPitchAngle = 95;
@@ -139,7 +137,7 @@ void LeapListener::onFrame(const Controller& controller) {
 	
 	//Bicep rot 20 - 180
 	int armYawAngle = arm.direction().yaw()*180/PI;
-	armYawAngle = (65 + armYawAngle)*1.12; //95 es el centro, y factor multiplicativo
+	armYawAngle = (65 + armYawAngle)*1.12;			//95 es el centro, y factor multiplicativo
 
 	if(armYawAngle > 180) {
 		armYawAngle = 180;
@@ -147,27 +145,28 @@ void LeapListener::onFrame(const Controller& controller) {
 	if(armYawAngle < 21) {
 		armYawAngle = 20;
 	}
-	armYawAngle = 180 - armYawAngle; //Darle vuelta al angulo
+	armYawAngle = 180 - armYawAngle;				//Darle vuelta al angulo
 
 
 	//Shoulder 30 - 90 (65 por cables de momento)
-	int elbowposx =  arm.elbowPosition()[0]; //posicion del codo en x esta entre [-300,-200]
+	int elbowposx =  arm.elbowPosition()[0];		//Posicion del codo en x esta entre [-300,-200]
 	int shoulderAngle;
-	aux = abs(elbowposx) - 200; //volverlo un valor entre [0, 160]
+	aux = abs(elbowposx) - 200;						//Volverlo un valor entre [0, 160]
 	if(aux < 0) {
 		aux = 0;
 	} else if (aux > 160) {
 		aux = 160;
 	}
-	shoulderAngle = aux * 0.63; //factor multiplicativo para que quede entre [30,90]
+	shoulderAngle = aux * 0.63;						//Factor multiplicativo para que quede entre [30,90]
 	if(shoulderAngle < 30){
 		shoulderAngle = 30;
 	} else if (shoulderAngle > 65) {
 		shoulderAngle = 65;	
 	}
+
 	//Hand roll 0 - 180
 	int handRollAngle = hand.palmNormal().roll()*180/PI;
-	if(handRollAngle < 0){ //ignorar la parte negativa 
+	if(handRollAngle < 0){							//Ignorar la parte negativa 
 		handRollAngle = abs(handRollAngle);
 		if(handRollAngle < 91) {
 			handRollAngle = 0;
@@ -175,48 +174,48 @@ void LeapListener::onFrame(const Controller& controller) {
 			handRollAngle = 180;
 		}
 	}
-	handRollAngle = 180 - handRollAngle; //Complemento del angulo
+	handRollAngle = 180 - handRollAngle;			//Complemento del angulo
 
+	/*
 	std::cout << std::string(2, ' ') 
-			  
-			  
-			  <<  "Elbow pitch: " << armPitchAngle << "\n"
-			  << " Bicep yaw: " << armYawAngle << "\n"	
-			  << "Arm angle " << armAngle<<"\n"
-			  << "Shoulder angle " << shoulderAngle << "\n"
-			  << " Hand roll: " << handRollAngle <<"\n"
-
+			  << "EP\t" << armPitchAngle << "\t"
+			  << "BY\t" << armYawAngle << "\t"	
+			  << "AA\t" << armAngle <<"\t"
+			  << "SA\t" << shoulderAngle << "\t"
+			  << "W\t" << handRollAngle << "\t"
 			  << std::endl;
-		char c, c1,c2,c3,c4, c5; //Para escribir al arduino
+	*/
+
+	char c, c1,c2,c3,c4, c5;						//Para escribir al Arduino
 		
-		// Get fingers
-		const FingerList fingers = hand.fingers();
-
-		int counter = 0;
+	// Get fingers
+	const FingerList fingers = hand.fingers();
+	int counter = 0;
 			
-		for (FingerList::const_iterator fl = fingers.begin(); fl != fingers.end(); ++fl) {
-			char c;
-			const Finger finger = *fl;
+	for (FingerList::const_iterator fl = fingers.begin(); fl != fingers.end(); ++fl) {
+		char c;
+		const Finger finger = *fl;
 
-			/*
-			std::cout << std::string(1, ' ') 
-					<<  fingerNames[finger.type()]
-					<< std::endl;
-			*/
+		/*
+		std::cout << std::string(1, ' ') 
+				<<  fingerNames[finger.type()]
+				<< std::endl;
+		*/
 
-			// Get finger bones
-			Leap::Vector vectors[3];	
-			int angle, angle1, angle2;
-			//Version angulos completos
-			/*
-			if(fl == fingers.begin()){ //Angulo dedo gordo
-				Bone::Type boneType = static_cast<Bone::Type>(1);
-				Bone bone = finger.bone(boneType);
-				vectors[0] = bone.direction();
+		// Get finger bones
+		Leap::Vector vectors[3];	
+		int angle, angle1, angle2;
+		
+		//Version angulos completos
+		/*
+		if(fl == fingers.begin()){ //Angulo dedo gordo
+			Bone::Type boneType = static_cast<Bone::Type>(1);
+			Bone bone = finger.bone(boneType);
+			vectors[0] = bone.direction();
 				
-				boneType = static_cast<Bone::Type>(2);
-				bone = finger.bone(boneType);
-				vectors[1] = bone.direction();
+			boneType = static_cast<Bone::Type>(2);
+			bone = finger.bone(boneType);
+			vectors[1] = bone.direction();
 				
 				boneType = static_cast<Bone::Type>(3);
 				bone = finger.bone(boneType);
@@ -248,6 +247,7 @@ void LeapListener::onFrame(const Controller& controller) {
 				angle = 180;
 			}
 			*/
+			
 			//Version abierto cerrado
 			if(finger.isExtended()) {
 				angle = 0;
@@ -267,18 +267,24 @@ void LeapListener::onFrame(const Controller& controller) {
 
 			counter++;
 		} //for end: FingerList iterator
-		//!!!!!!!!!!!!CHECK THIS DATA!!!!!!!!!!!!!!!!!!
+		
 		c1 = (char)(armPitchAngle);
 		c2 = (char)(armYawAngle);
 		c3 = (char)(armAngle);
 		c4 = (char)(shoulderAngle);
 		c5 = (char)(handRollAngle);
+		/*
 		SP ->WriteData(&c1,1);	 
 		SP ->WriteData(&c2,1);
 		SP ->WriteData(&c3,1);
 		SP ->WriteData(&c4,1);
 		SP ->WriteData(&c5,1);
-		//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		*/
+		armAngles[0] = c1;
+		armAngles[1] = c2;
+		armAngles[2] = c3;
+		armAngles[3] = c4;
+		armAngles[4] = c5;
 	} //for end: HandList iterator
 
 	if (!frame.hands().isEmpty()) {
@@ -324,16 +330,15 @@ int Init () {
 	namedWindow("inMoov",CV_WINDOW_NORMAL);
 	cvSetWindowProperty( "inMoov", CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN );
 	cvResizeWindow("inMoov", 1080, 1920);
-	cvMoveWindow("inMoov", 1601, 0);
+	cvMoveWindow("inMoov", 1601, 0);		//The Oculus is set as an extended monitor
+											//My main monitor is 1600px in width, with this position the new window is forced to the second monitor
 
 	/* Window for main monitor */
 	namedWindow("main monitor",CV_WINDOW_AUTOSIZE);
-	cvMoveWindow("main monitor", 0, 0);
+	cvMoveWindow("main monitor", 100, 100);
 
 	/* Connect to serial */
-	SP = new Serial("\\\\.\\COM43");	//TO DO: pedir numero de puerto en consola
-
-	//comentario
+	SP = new Serial("\\\\.\\COM43");		//TO DO: pedir numero de puerto en consola
 
 	if (SP->IsConnected() == false) {
 		return -2;
@@ -351,24 +356,23 @@ int Loop () {
 	Mat frameL, frameR;
 	Mat composition;
 
-	int delay = 1000/fps;
+	int delay = 15;
 	int frame = 0;
 	int counter = 10;
 
 	char cYaw;
 	char cPitch;
-	//char cRoll;
 
 	while (true) {
 		/* Get orientation as a Quaternion */
 		ovrHmd_BeginFrameTiming(hmd, frame);
 		ovrTrackingState trackState = ovrHmd_GetTrackingState(hmd, ovr_GetTimeInSeconds());
-		ovrQuatf curOrient = trackState.HeadPose.ThePose.Orientation;
+		ovrQuatf curOrient = trackState.HeadPose.ThePose.Orientation;4
 		ovrHmd_EndFrameTiming(hmd);
 		frame++;
 
 		/* Convert to Euler angles */
-		float eyeYaw, eyePitch, eyeRoll;
+		float eyeYaw, eyePitch, eyeRoll; 
 		OVR::Quatf l_Orientation = OVR::Quatf(curOrient);
 		l_Orientation.GetEulerAngles<OVR::Axis_Y, OVR::Axis_X, OVR::Axis_Z>(&eyeYaw, &eyePitch, &eyeRoll);
 		eyeYaw		= OVR::RadToDegree(eyeYaw);
@@ -387,7 +391,7 @@ int Loop () {
 			if ((eyePitch+90) > 10 && (eyePitch+90) < 170) {
 				if (eyeYaw >= 0 && eyeYaw <= 180) {
 					cYaw = (char)eyeYaw;
-				} else if (eyeYaw > -90) {
+				} else if (eyeYaw > -90) {			//TO DO: Revisar estos rangos
 					cYaw = (char)0;
 				} else if (eyeYaw < -90) {
 					cYaw = (char)180;
@@ -397,9 +401,6 @@ int Loop () {
 			} else {
 				cPitch = (char)0;
 			}
-
-			/* Roll */
-			//unused
 
 			SP->WriteData(&cYaw, 1);
 			SP->WriteData(&cPitch, 1);
@@ -412,6 +413,10 @@ int Loop () {
 					fingerAngles[arrIndex] = (originalAngles[arrIndex] < 70) ? 0 : 180;
 				//}
 				c = fingerAngles[arrIndex];
+				SP->WriteData(&c, 1);
+			}
+			for (int arrIndex = 0; arrIndex < 5; arrIndex++) {
+				c = armAngles[arrIndex];
 				SP->WriteData(&c, 1);
 			}
 
@@ -427,7 +432,6 @@ int Loop () {
 				<< endl;
 			*/
 
-			
 			cout << "Yaw: " << (int)cYaw 
 				<< "\tPitch: " << (int)cPitch 
 				<< "\tFingers: " 
@@ -437,6 +441,14 @@ int Loop () {
 					<< (int)fingerAngles[3] << "\t"
 					<< (int)fingerAngles[4] << "\t"
 				<< endl;
+
+			cout << std::string(2, ' ') 
+			  << "BI\t" << (int)armAngles[0] << "\t"
+			  << "HY\t" << (int)armAngles[1] << "\t"	
+			  << "AA\t" << (int)armAngles[2] <<"\t"
+			  << "SA\t" << (int)armAngles[3] << "\t"
+			  << "W \t" << (int)armAngles[4] << "\t"
+			  << std::endl;
 			
 			//counter = 1;
 		//}
@@ -462,9 +474,9 @@ int Loop () {
 		frameR.copyTo (right);
 
 		/* Make image for main monitor */
-		Mat matRotate		(500,500,frameL.type());
-		Mat matMainMonitor	(500,500,frameL.type());
-		resize(frameR, matRotate, Size(500,500), 0,0, INTER_CUBIC);
+		Mat matRotate		(850,850,frameL.type());
+		Mat matMainMonitor	(850,850,frameL.type());
+		resize(frameR, matRotate, Size(850,850), 0,0, INTER_CUBIC);
 		transpose(matRotate, matMainMonitor);							//cameras in the robot are rotated
 		flip(matMainMonitor, matMainMonitor, 0);
 
--- Other/complete/complete.ino
+++ Other/complete/complete.ino
@@ -1,21 +1,40 @@
 #include <Servo.h>
 
-const int NUMBER_OF_SERVOS = 7;
+const int NUMBER_OF_SERVOS = 12;
+
+const int HDPIT =  6;
+const int HDYAW =  7;
+
+const int THUMB = 14;
+const int INDEX = 15;
+const int MIDDL = 16;
+const int RINGF = 17;
+const int PINKY = 18;
+
+const int CLAVI =  9;
+const int SHYAW = 10;
+const int SHPIT = 11;
+const int BICEP = 12;
+const int WRIST = 13;
+
 Servo allServos[NUMBER_OF_SERVOS];
 
 int incomingByte = 0;
 
 void setup() {
   Serial.begin(115200);
-  allServos[0].attach( 7);  //interior de cabeza (yaw)
-  allServos[1].attach( 6);  //piston del cuello (pitch)
-  
-  /* dedos: reciben 180 para estirar y 0 para contraer */
-  allServos[2].attach(14);  //pulgar
-  allServos[3].attach(15);  //indice
-  allServos[4].attach(16);  //medio
-  allServos[5].attach(17);  //anular
-  allServos[6].attach(18);  //menique
+  allServos[0].attach(HDYAW);
+  allServos[1].attach(HDPIT);
+  allServos[2].attach(THUMB);
+  allServos[3].attach(INDEX);
+  allServos[4].attach(MIDDL);
+  allServos[5].attach(RINGF);
+  allServos[6].attach(PINKY);
+  allServos[7].attach(BICEP);
+  allServos[8].attach(SHYAW);
+  allServos[9].attach(SHPIT);
+  allServos[10].attach(CLAVI);
+  allServos[11].attach(WRIST);
 }
 
 void loop() {
@@ -26,7 +45,12 @@ void loop() {
     while(Serial.available() == 0);
     incomingByte = Serial.read();
     
-    if (i == 2 || i == 3 || i == 4) {  //dedos: pulgar, indice y medio tienen servo al reves
+    /*
+      will get a number between 0 and 180 for fingers, 0 is open and 180 is closed
+      thumb, index and middle are connected backwards
+      the Leap reader will take care of the values for the arm servos
+    */
+    if (i == 2 || i == 3 || i == 4) {
       allServos[i].write(180 - incomingByte);
     } else {
       allServos[i].write(incomingByte);
